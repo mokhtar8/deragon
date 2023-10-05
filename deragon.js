58de6d10cb80cc44b4c93496c32ca876c8cf1bca
mokhtar8 <m0kht4armm@gmail.com>
2023-10-05 21:49:44 +0330

    fifth commit

--- deragon.js
+++ deragon.js
@@ -1,11 +1,18 @@
 var img = document.getElementById("pokemonimage");
+var numberforinput = document.getElementById('inputnumber');
 
+numberforinput.addEventListener('input', function () {
+var numbers = numberforinput.value;
+  let userInput = parseFloat(numberforinput.value);
 
-function calll() {
-
-  var number = document.getElementById('inputnumber');
-  var numbers = number.value;
-
+  if (userInput < 1 ||userInput > 20) {
+    alert('عددی بین 1 تا 20 را وارد کنید')
+    numberforinput.value = '';
+  } else {
+    designcall(numbers)
+  }
+});
+function designcall(numbers) {
 
   fetch(`https://pokeapi.co/api/v2/pokemon/${numbers}`)
     .then(response => response.json())
@@ -15,53 +22,23 @@ function calll() {
       img.src = data.sprites.front_default;
       document.getElementById("text").innerHTML = data.name;
 
-
-
-      document.getElementById("imgpok").src =data.sprites.other["official-artwork"].front_default;
+      document.getElementById("deraonimage2").src =data.sprites.other["official-artwork"].front_default;
      document.getElementById("number").innerHTML = '00'+ data.id;
       document.getElementById("textbulb").innerHTML = data.name;
       document.getElementById("wartype").innerHTML = data.types[0].type.name;
       document.getElementById("weight").innerHTML = data.weight;
       document.getElementById("height").innerHTML = data.height;
-      caller ()
+      caller (numbers)
    
     })
     .catch(error => console.log(error));
-
-
-  number.addEventListener('input', function () {
-
-    let userInput = parseInt(number.value);
-
-    if (userInput < 1) {
-      alert('عددی بین 1 تا 20 را وارد کنید')
-      userInput = '';
-    } else if (userInput > 20) {
-      alert('عددی بین 1 تا 20 را وارد کنید')
-      userInput = '';
-    }
-
-    number.value = userInput;
-  });
-
-
-
 }
-
-
-
-function caller () {
-
-  var number = document.getElementById('inputnumber');
-  var numbers = number.value;
-
+function caller (numbers) {
 
   fetch(`https://pokeapi.co/api/v2/pokemon-species/${numbers}`)
     .then(response => response.json())
     .then(data => {
       console.log(data)
-
-  
 
       document.getElementById("gender").innerHTML = data.genera[7].genus;
       document.getElementById("color").innerHTML = data.color.name;
@@ -72,16 +49,13 @@ function caller () {
 //change diplay// 
 
 var open = document.getElementById("mylink");
-var box = document.getElementById("changer")
+var box = document.getElementById("bodychanger")
 open.addEventListener('click', function()  {
 
   if(box.style.display == "none") {
     box.style.display = 'block';
   } else {
     
-    
     box.style.display = 'none';
-    
   }
-
 });
